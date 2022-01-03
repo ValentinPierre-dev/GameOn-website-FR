24d0af2ea4490aa7fc5c0510a54b849c3cd427fa
Valentin Pierre <valentin.pierre71@gmail.com>
2022-01-03 16:35:40 +0100

    2 caractères minimum pour le nom et le prénom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,11 +13,14 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCls = document.querySelectorAll(".close");
 
-const firstName = document.getElementById("#first");
-const lastName = document.getElementById("#last");
-const eMail = document.getElementById("#email");
-const birthDate = document.getElementById("#birthdate");
-const nbTournament = document.getElementById("#quantity");
+const textControl = document.querySelectorAll(".text-control");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const eMail = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const nbTournament = document.getElementById("quantity");
+
+const firstError = document.getElementById("first-error");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -35,4 +38,47 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Validate Inputs
+function validateTxtCtrl() {
+  if (textControl.value !== null){
+    return true;
+  }
+  else {
+    alert("Merci de remplir les champs obligatoires");
+    return false;
+  }
+}
+
+function validateFirstName() {
+  if (firstName.value.length >= 2) {
+    firstError.innerText = "";
+    return true;
+  }
+  else {
+    firstError.innerText = "Merci de rentrer au moins 2 caractères";
+    return false;
+  }
+}
+
+function validateLastName() {
+  if (lastName.value.length >= 2) {
+    firstError.innerText = "";
+    return true;
+  }
+  else {
+    firstError.innerText = "Merci de rentrer au moins 2 caractères";
+    return false;
+  }
+}
+
+// Validate Form
+function validate(e) {
+  e.preventDefault();
+  //validateTxtCtrl();
+  const firstNameIsValid = validateFirstName();
+  validateLastName();
+  if (firstNameIsValid){
+    alert ("Inscription envoyée");
+  }
+}
 

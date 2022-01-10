c84b17a8144a88382994ccbbef3876d00fa093e4
Valentin Pierre <valentin.pierre71@gmail.com>
2022-01-10 12:15:58 +0100

    Ajout d'animation sur les erreurs et opacité sur le bouton d'envoi

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,9 +13,9 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCls = document.querySelectorAll(".close");
 const modalBody = document.querySelectorAll(".modal-body");
-const modalSub = document.getElementById("disable-btn");
-const suscribeBtn = document.getElementById("suscribe-btn");
 
+const suscribeForm = document.getElementById("suscribe-form");
+const modalSub = document.getElementById("disable-btn");
 const modalCont = document.getElementById("modal-content");
 
 const firstName = document.getElementById("first");
@@ -46,12 +46,29 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCls.forEach((cls) => cls.addEventListener("click", closeModal));
 
 firstName.addEventListener("change", validateFirstName);
-lastName.addEventListener("change", validateLastName);
-eMail.addEventListener("change", validateMail);
-birthDate.addEventListener("change", validateBirthDate);
-nbTournament.addEventListener("change", validateTournament);
+firstName.addEventListener("change", enableBtn);
 
-suscribeBtn.addEventListener("click", disableModal);
+lastName.addEventListener("change", validateLastName);
+lastName.addEventListener("change", enableBtn);
+
+eMail.addEventListener("change", validateMail);
+lastName.addEventListener("change", enableBtn);
+
+birthDate.addEventListener("change", validateBirthDate);
+lastName.addEventListener("change", enableBtn);
+
+nbTournament.addEventListener("change", validateTournament);
+nbTournament.addEventListener("change", enableBtn);
+
+location1.addEventListener("change", enableBtn);
+location1.addEventListener("change", enableBtn);
+location2.addEventListener("change", enableBtn);
+location3.addEventListener("change", enableBtn);
+location4.addEventListener("change", enableBtn);
+location5.addEventListener("change", enableBtn);
+location6.addEventListener("change", enableBtn);
+
+checkbox.addEventListener("change", enableBtn);
 
 // launch modal form
 function launchModal() {
@@ -72,6 +89,7 @@ function validateFirstName() {
   }
   else {
     firstName.style.border = "2px solid red";
+    firstName.style.animation = "headshake 100ms cubic-bezier(.4,.1,.6,.9)";
     firstError.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     return false;
   }
@@ -86,6 +104,7 @@ function validateLastName() {
   }
   else {
     lastName.style.border = "2px solid red";
+    lastName.style.animation = "headshake 100ms cubic-bezier(.4,.1,.6,.9)";
     lastError.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     return false;
   }
@@ -100,6 +119,7 @@ function validateMail() {
   }
   else {
     eMail.style.border = "2px solid red";
+    eMail.style.animation = "headshake 100ms cubic-bezier(.4,.1,.6,.9)";
     mailError.innerText = "Veuillez entrer une adresse mail valide.";
     return false;
   }
@@ -114,6 +134,7 @@ function validateBirthDate() {
   }
   else {
     birthDate.style.border = "2px solid red";
+    birthDate.style.animation = "headshake 100ms cubic-bezier(.4,.1,.6,.9)";
     birthError.innerText = "Vous devez entrer une date de naissance valide.";
     return false;
   }
@@ -128,6 +149,7 @@ function validateTournament() {
   }
   else {
     nbTournament.style.border = "2px solid red";
+    nbTournament.style.animation = "headshake 100ms cubic-bezier(.4,.1,.6,.9)";
     tournamentError.innerText = "Veuillez entrer une valeur numérique valide.";
     return false;
   }
@@ -167,23 +189,97 @@ function validate(e) {
   const nbTournamentIsValid = validateTournament();
   const locationsIsValid = validateLocations();
   const checkboxIsValid = validateCheckbox();
-  const suscribeForm = document.getElementById("suscribe-form");
   if (firstNameIsValid && lastNameIsValid && eMailIsValid && birthDateIsValid && nbTournamentIsValid && locationsIsValid && checkboxIsValid){
     modalCont.innerHTML = "Merci ! Votre réservation a été reçue.";
     suscribeForm.reset();
   }
 }
 
-// Disable submit button
-function disableModal() {
-  const firstNameIsValid = validateFirstName();
-  const lastNameIsValid = validateLastName();
-  const eMailIsValid = validateMail();
-  const birthDateIsValid = validateBirthDate();
-  const nbTournamentIsValid = validateTournament();
-  const locationsIsValid = validateLocations();
-  const checkboxIsValid = validateCheckbox();
-  if (firstNameIsValid && lastNameIsValid && eMailIsValid && birthDateIsValid && nbTournamentIsValid && locationsIsValid && checkboxIsValid){
-    modalSub.style.opacity = "0.2";
+
+
+// First Name for enable button
+function enableFirstName() {
+  if (firstName.value.length >= 2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Last Name for enable button
+function enableLastName() {
+  if (lastName.value.length >= 2) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// E-Mail for enable button
+function enableMail() {
+  if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(eMail.value)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Birth date for enable button
+function enableBirthDate() {
+  if (/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(birthDate.value)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Tournament quantity for enable button
+function enableTournament() {
+  if (/^[1-9][0-9]?$/.test(nbTournament.value)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Locations for enable button
+function enableLocations() {
+  if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
+    return false;
+  }
+  else {
+    locationError.innerText = "";
+    return true;
+  }
+}
+
+// Checkbox for enable button
+  function enableCheckbox() {
+  if (!checkbox.checked) {
+    return false;
+  }
+  else {
+    checkboxError.innerText = "";
+    return true;
+  }
+}
+
+// Enable submit button
+function enableBtn() {
+  const firstNameIsEnable = enableFirstName();
+  const lastNameIsEnable = enableLastName();
+  const eMailIsEnable = enableMail();
+  const birthDateIsEnable = enableBirthDate();
+  const nbTournamentIsEnable = enableTournament();
+  const locationsIsEnable = enableLocations();
+  const checkboxIsEnable = enableCheckbox();
+  if (firstNameIsEnable && lastNameIsEnable && eMailIsEnable && birthDateIsEnable && nbTournamentIsEnable && locationsIsEnable && checkboxIsEnable){
+    modalSub.style.opacity = "1";
+    modalSub.style.cursor = "pointer";
   }
 }
